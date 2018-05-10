2d9c1b79e72abe484291f6d2845d0dfd430bc178
Ashley Schultz <ashleyeschultz@gmail.com>
2018-05-10 01:00:53 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,10 @@
 module Memorable
-  
-  def self.reset_all
-    self.all.clear
-  end
-  
-  def self.count
-    @@artists.count
-  end
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
+    
+    def self.count
+      @@artists.count
+    end
 end 
